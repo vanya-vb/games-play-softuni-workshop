e9ddf3a12705074f3880ec3d87f38798ac915b9d
vanya-vb <vbozadzhieva29@gmail.com>
2025-07-21 12:54:24 +0300

    add main pages and routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,10 @@ import { Routes, Route } from 'react-router'
 
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
+import Login from './components/Login/Login'
+import Register from './components/Register/Register'
+import GameCreate from './components/GameCreate/GameCreate'
+import GameCatalog from './components/GameCatalog/GameCatalog'
 import './App.css'
 
 function App() {
@@ -13,6 +17,10 @@ function App() {
       <main id="main-content">
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/games' element={<GameCatalog />} />
+          <Route path='/games/create' element={<GameCreate />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
         </Routes>
 
       </main>
