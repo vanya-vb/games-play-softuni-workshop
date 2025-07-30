121f038e22ff641688a43bfd34ffa649ca18d3ac
vanya-vb <vbozadzhieva29@gmail.com>
2025-07-30 13:57:04 +0300

    fix 204 no content response

--- client/src/utils/request.js
+++ client/src/utils/request.js
@@ -16,6 +16,12 @@ const request = async (method, url, data, options = {}) => {
     }
 
     const response = await fetch(url, options);
+    const responseContentType = response.headers.get('Content-Type');
+    
+    if (!responseContentType) {
+        return;
+    }
+
     const result = await response.json();
 
     return result;
