a3dbc39c10a3cfbbef16483a94a6ca6366c68ec8
vanya-vb <vbozadzhieva29@gmail.com>
2025-07-30 13:15:51 +0300

    add user register

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -1,8 +1,33 @@
+import { useContext } from "react";
+import { useRegister } from "../../api/authApi";
+import { UserContext } from "../../contexts/UserContext";
+import { useNavigate } from "react-router";
+
 export default function Register() {
+    const navigate = useNavigate();
+    const { register } = useRegister();
+    const { userLoginHandler } = useContext(UserContext);
+
+    const registerHandler = async (formData) => {
+        const { email, password } = Object.fromEntries(formData);
+
+        const confirmPassword = formData.get('confirm-password');
+
+        if (password !== confirmPassword) {
+            console.log('Password missmatch');
+
+            return;
+        }
+
+        const authData = await register(email, password);
+        userLoginHandler(authData);
+        navigate('/');
+    }
+
     return (
         // < !--Register Page(Only for Guest users ) -->
         <section id="register-page" className="content auth">
-            <form id="register">
+            <form id="register" action={registerHandler}>
                 <div className="container">
                     <div className="brand-logo"></div>
                     <h1>Register</h1>
