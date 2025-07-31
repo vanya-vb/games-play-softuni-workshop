015abf87ae75de78548e3b50ffe2e108235d0c12
vanya-vb <vbozadzhieva29@gmail.com>
2025-07-31 18:40:07 +0300

    replace basic auth guard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,30 +11,32 @@ import GameCatalog from './components/GameCatalog/GameCatalog'
 import GameDetails from './components/GameDetails/GameDetails'
 import GameEdit from './components/GameEdit/GameEdit'
 import Logout from './components/Logout/Logout'
-import AuthGuard from './components/guards/AuthGuardBasic'
+import AuthGuard from './components/guards/AuthGuard'
 
 function App() {
-  return (
-    <UserProvider>
-      <div id="box">
-        <Header />
+    return (
+        <UserProvider>
+            <div id="box">
+                <Header />
 
-        <main id="main-content">
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/games' element={<GameCatalog />} />
-            <Route path='/games/create' element={<AuthGuard><GameCreate /></AuthGuard>} />
-            <Route path='/games/:gameId/details' element={<GameDetails />} />
-            <Route path='/games/:gameId/edit' element={<GameEdit />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/logout' element={<Logout />} />
-          </Routes>
+                <main id="main-content">
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/games' element={<GameCatalog />} />
+                        <Route path='/games/:gameId/details' element={<GameDetails />} />
+                        <Route element={<AuthGuard />}>
+                            <Route path='/games/create' element={<GameCreate />} />
+                            <Route path='/games/:gameId/edit' element={<GameEdit />} />
+                            <Route path='/logout' element={<Logout />} />
+                        </Route>
+                        <Route path='/login' element={<Login />} />
+                        <Route path='/register' element={<Register />} />
+                    </Routes>
 
-        </main>
-      </div>
-    </UserProvider>
-  )
+                </main>
+            </div>
+        </UserProvider>
+    )
 }
 
 export default App
